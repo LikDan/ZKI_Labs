a33ca438ae6aa75acf437d707769f991e7ab914e
LikDan <likdan.company@gmail.com>
2022-11-10 22:00:26 +0300

    Initial Commit

---
+++ Encrypt/EncryptApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncryptApp.swift
+//  Encrypt
+//
+//  Created by Lik Dan on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct EncryptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
